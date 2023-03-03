7fe3865d9b1df21d7c819f72582d665c381158f6
Ishan Gupta <ishan.d.gupta@outlook.com>
2023-03-03 20:17:02 +0530

    registerRoutes.js - router post async await.

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -13,7 +13,7 @@ router.get('/', (req, res, next) => {
     res.status(200).render('register');
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
     var firstName = req.body.firstName.trim();
     var lastName = req.body.lastName.trim();
     var username = req.body.username.trim();
@@ -21,20 +21,17 @@ router.post('/', (req, res, next) => {
     var password = req.body.password;
     var payload = req.body;
     if (firstName && lastName && username && email && password) {
-        User.findOne({
+        var user = await User.findOne({
             $or: [
                 { username: username },
                 { email: email }
             ]
-        })
-        .then((user)=>{
-            console.log('value is: ' + user);
         })
     }
     else {
         payload.errorMessage = 'Make sure each field has a valid value';
         res.status(200).render('register', payload);
     }
-})
+});
 
 module.exports = router;
